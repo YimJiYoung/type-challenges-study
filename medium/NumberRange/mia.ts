391eaa23c9e55039d851c54bd01a0009ddc5fff8
YimJiYoung <chiy323@gmail.com>
2022-05-11 22:11:08 +0900

    NumberRange 문제 풀이

---
+++ medium/NumberRange/mia.ts
@@ -0,0 +1,14 @@
+// SequenceImpl<N, [], false> : [0, ..., N-1] 배열 타입
+// SequenceImpl<N, [], true> : [0, ..., N] 배열 타입
+type Sequence<N extends number, Seq extends number[], includeN extends boolean> = Seq['length'] extends N
+  ? includeN extends true
+    ? [...Seq, Seq['length']]
+    : Seq
+  : Sequence<N, [...Seq, Seq['length']], includeN>;
+
+type ValuesOf<T extends any[]> = T[number];
+
+type NumberRange<L extends number, H extends number> = Exclude<
+ValuesOf<Sequence<H, [], true>>,
+ValuesOf<Sequence<L, [], false>>
+>;
