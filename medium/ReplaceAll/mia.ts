db7fc7cba4b6910c28d09ab6fdfd3b6971cc8a81
Mia <37496919+YimJiYoung@users.noreply.github.com>
2022-05-15 13:36:48 +0900

    feat: ReplaceAll, Replace 구현

---
+++ medium/ReplaceAll/mia.ts
@@ -0,0 +1,22 @@
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type NonEmptyString<T extends string> = '' extends T ? never : T;
+type ReplaceAll<
+  S extends string,
+  From extends string,
+  To extends string
+> = S extends `${infer Left}${NonEmptyString<From>}${infer Right}`
+  ? `${ReplaceAll<Left, From, To>}${To}${ReplaceAll<Right, From, To>}`
+  : S;
+
+type cases = [
+  Expect<Equal<ReplaceAll<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobar', 'bag', 'foo'>, 'foobar'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', 'bar', 'foo'>, 'foofoofoo'>>,
+  Expect<Equal<ReplaceAll<'t y p e s', ' ', ''>, 'types'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<ReplaceAll<'barfoo', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobarfoobar', 'ob', 'b'>, 'fobarfobar'>>,
+  Expect<Equal<ReplaceAll<'foboorfoboar', 'bo', 'b'>, 'foborfobar'>>,
+  Expect<Equal<ReplaceAll<'', '', ''>, ''>>
+];
