7f9267501f8da3b11c4f64b21dece2556b911f76
kang-heewon <heewon.dev@gmail.com>
2022-05-19 14:43:27 +0900

    구현

---
+++ medium/ReplaceAll/owen.ts
@@ -0,0 +1,17 @@
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type ReplaceAll<S extends string, From extends string, To extends string> = S extends `${infer Front}${From}${infer Back}` ? `${Front}${To}${Back}` : S;
+
+type Hello = ReplaceAll<'hello', 'h', 'b'>;
+
+type cases = [
+  Expect<Equal<ReplaceAll<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobar', 'bag', 'foo'>, 'foobar'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', 'bar', 'foo'>, 'foofoofoo'>>,
+  Expect<Equal<ReplaceAll<'t y p e s', ' ', ''>, 'types'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<ReplaceAll<'barfoo', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobarfoobar', 'ob', 'b'>, 'fobarfobar'>>,
+  Expect<Equal<ReplaceAll<'foboorfoboar', 'bo', 'b'>, 'foborfobar'>>,
+  Expect<Equal<ReplaceAll<'', '', ''>, ''>>
+];
