e694423aacf1b627352bd3dc8c6ae363224c6a77
YimJiYoung <chiy323@gmail.com>
2022-05-25 23:41:10 +0900

    InorderTraversal 타입 구현

---
+++ medium/InorderTraversal/mia.ts
@@ -0,0 +1,59 @@
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+interface TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+}
+type InorderTraversal<T extends TreeNode | null> = [T] extends [TreeNode]
+  ? [...InorderTraversal<T['left']>, T['val'], ...InorderTraversal<T['right']>]
+  : [];
+
+const tree1 = {
+  val: 1,
+  left: null,
+  right: {
+    val: 2,
+    left: {
+      val: 3,
+      left: null,
+      right: null,
+    },
+    right: null,
+  },
+} as const;
+
+const tree2 = {
+  val: 1,
+  left: null,
+  right: null,
+} as const;
+
+const tree3 = {
+  val: 1,
+  left: {
+    val: 2,
+    left: null,
+    right: null,
+  },
+  right: null,
+} as const;
+
+const tree4 = {
+  val: 1,
+  left: null,
+  right: {
+    val: 2,
+    left: null,
+    right: null,
+  },
+} as const;
+
+type cases = [
+  Expect<Equal<InorderTraversal<null>, []>>,
+  Expect<Equal<InorderTraversal<typeof tree1>, [1, 3, 2]>>,
+  Expect<Equal<InorderTraversal<typeof tree2>, [1]>>,
+  Expect<Equal<InorderTraversal<typeof tree3>, [2, 1]>>,
+  Expect<Equal<InorderTraversal<typeof tree4>, [1, 2]>>
+];
